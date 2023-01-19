0f9ef4b0dc9d0bf54889c5f7d3d68bb30dee95af
Patrick Leiser <patrick27leiser@yahoo.com>
2023-01-19 12:39:12 -0800

    add download script for stage 2

---
+++ data/download_data.sh
@@ -0,0 +1,5 @@
+# derived from the link https://drive.google.com/file/d/1WG0NV7JTcDmpcEB-GRf-qecyWTisFYM-/view
+wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=1WG0NV7JTcDmpcEB-GRf-qecyWTisFYM-' -O 'stage_2_data.zip'
+unzip stage_2_data.zip
+rm stage_2_data.zip 
+rm -r __MACOSX
