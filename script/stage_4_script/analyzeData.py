a5c4ab05b79d346a5b6feae8faa6a921668ce2c7
Patrick Leiser <patrick27leiser@yahoo.com>
2023-02-28 15:31:34 -0800

    add joke processing to analyzeData.py

--- script/stage_4_script/analyzeData.py
+++ script/stage_4_script/analyzeData.py
@@ -31,14 +31,37 @@ def scanDirectory(dirName, fileLimit = 0, sentiment = "unset", wordDict={}):
 	#print({k: v for k, v in sorted(wordDict.items(), key=lambda item: item[1])})
 	print("---- end scan of "+dirName+", "+str(fileCount)+" files scanned ----")
 	return wordDict
+	
+#for the jokes file, slightly different format than the movie reviews
+def scanFile(fileName, lineLimit = 0, wordDict={}):
+	print("---- begin scan of "+fileName+" ----")
+	lineCount = 0
+	for line in open(os.getcwd()+"/stage_4_data/"+fileName):
+		lineCount+=1
+		#print(line)
+		for word in line.split():
+			#print(word)
+			if(word in wordDict):
+				#wordDict[word][sentiment] += 1
+				wordDict[word]["count"] += 1
+			else:
+				if not word in wordDict:
+					wordDict[word]={}
+					wordDict[word]["count"] = 0
+				#wordDict[word][sentiment] = 1
+				wordDict[word]["count"] += 1
+		if (lineLimit and lineCount >= lineLimit):
+			break
+	print("---- end scan of "+fileName+", "+str(lineCount)+" lines scanned ----")
+	return wordDict
 
 #print the sorted results in the form:
 #indexNum, word, {count: totalCount, pos: posCount, neg: negCount}
 #stops after words with fewer than threshold occurrences, or after wordLimit (if nonzero) total words have been printed
-def printResults(wordDict, threshold=0, wordLimit=0, toFile=False):
+def printResults(wordDict, threshold=0, wordLimit=0, toFile=False, outFilePrefix=""):
 	outFile=None
 	if toFile:
-		outFile=open(os.getcwd()+"/generated_stage_4_data/thresh-"+str(threshold)+"_lim-"+str(wordLimit)+".txt", "w")
+		outFile=open(os.getcwd()+"/generated_stage_4_data/"+outFilePrefix+"thresh-"+str(threshold)+"_lim-"+str(wordLimit)+".txt", "w")
 	index = 0
 	for k, v in sorted(wordDict.items(), key=lambda item: item[1]["count"], reverse=True):
 		if v["count"] < threshold:
@@ -68,6 +91,10 @@ wordDict = scanDirectory("text_classification/train/neg", 0, "neg", wordDict)
 
 
 printResults(wordDict, 10, 0, True)
+
+jokeDict = scanFile("text_generation/data", 0, {})
+
+printResults(jokeDict, 0, 0, True, "jokes_")
 
 
 
